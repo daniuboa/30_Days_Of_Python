c79881fdaafd9e396b8c5874bb75e6cc1317d9ba
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 15:22:15 +0300

    Refering to variables

---
+++ Day-02-Strings_Variables_and_Getting_Input/variable_reference.py
@@ -0,0 +1,8 @@
+# Values can be retrieved from variables by writing the variable name where we want to use the value.
+
+# Example: 
+
+hourly_wage = 20.00
+hours_worked = 40
+
+print(hourly_wage * hours_worked)
