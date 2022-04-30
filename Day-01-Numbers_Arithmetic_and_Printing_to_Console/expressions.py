6ab599efd6f7cf513695c13f91c557a5c255c900
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 14:45:35 +0300

    Brief introduction to expressions

---
+++ Day-01-Numbers_Arithmetic_and_Printing_to_Console/expressions.py
@@ -0,0 +1,12 @@
+# An expresion evaluates to a value.
+
+# The expression is evaluated from left to right.
+# The value of the expression is the value of the last operand.
+
+# Examples of expressions:
+
+5 + 3
+
+print("Hello")
+
+n = 5
