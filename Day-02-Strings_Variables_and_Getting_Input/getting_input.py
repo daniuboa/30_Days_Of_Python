e5c1ba9dac4763336b3b98479ac3512e2071adfe
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 15:27:49 +0300

    Getting values from theuser

---
+++ Day-02-Strings_Variables_and_Getting_Input/getting_input.py
@@ -0,0 +1,6 @@
+# The input() function is used to get input from the user.
+ # Example:
+
+name = input("What is your name? ")
+
+# The above code prompts the user to enter their name and save it in the variable name.
