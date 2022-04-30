2d0cc887628a360ad90d1df46fe7daa7cf9e7936
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 14:53:14 +0300

    Arithetic operators

---
+++ Day-01-Numbers_Arithmetic_and_Printing_to_Console/arithmetic_operators.py
@@ -0,0 +1,40 @@
+# In python, we accomplish basic arithmetic through the use of arithmetic operators.
+# Arithmetic operators are used to perform basic arithmetic operations on numbers.
+
+# Operator symbols include '+', '-', '*', '/', '//', '%', '**', '&', '|', '^', '>>', '<<'
+
+# Addition operators
+# Addition operator '+' is used to add two numbers.
+# The result of addition is always a number.
+# The result of addition is the sum of the two operands.
+
+print( 5 + 3 )
+print(6.4 + 2.3)
+print(8 + 2.0)
+
+# Subtraction operators
+# Subtraction operator '-' is used to subtract two numbers.
+# The result of subtraction is always a number.
+# The result of subtraction is the difference of the two operands.
+
+print( 5 - 3 )
+print(6.4 - 2.3)
+print(8 - 2.0)
+
+# Multiplication operators
+# Multiplication operator '*' is used to multiply two numbers.
+# The result of multiplication is always a number.
+# The result of multiplication is the product of the two operands.
+
+print(6 * 3)
+print(6.4 * 2.3)
+print(8 * 2.0)
+
+# Division operators
+# Division operator '/' is used to divide two numbers.
+# The result of division is always a number.
+# The result of division is the quotient of the two operands.
+
+print(4 / 2)
+print(6.4 / 2.3)
+print(8 / 2.0)
