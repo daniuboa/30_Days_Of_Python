f45b3e19947ac23b2d60d5e79f273da2aa4b5437
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 15:13:12 +0300

    String basics

---
+++ Day-02-Strings_Variables_and_Getting_Input/string_basics.py
@@ -0,0 +1,8 @@
+# Strings in python are ordered sequences of zero or more characters, and we can use them to represent arbitrary collections of symbols.
+# In order to create string, we need to wrap some series of characters in quotation marks.
+
+# Example of a string:
+
+"This is a string."
+
+'This is also a valid string.'
