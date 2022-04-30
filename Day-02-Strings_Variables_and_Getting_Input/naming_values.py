a9f5d28e446ed97be3e9188ab904bdc076cba737
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 15:18:58 +0300

    Naming values

---
+++ Day-02-Strings_Variables_and_Getting_Input/naming_values.py
@@ -0,0 +1,13 @@
+# We can name values in python using the asignment operation.
+
+# Exampes:
+name = "Dani Uboa"
+age = 100
+
+#The variable names we can use are entirely up to us, but ther are some rules we must follow:
+# 1. The variable name must start with a letter or an underscore.
+# 2. The variable name can only contain letters, numbers, and underscores.
+# 3. The variable name is case sensitive.
+# 4. The variable name must be unique.
+# 5. The variable name cannot start with a number.
+# 6. The variable name cannot be a python keyword.
