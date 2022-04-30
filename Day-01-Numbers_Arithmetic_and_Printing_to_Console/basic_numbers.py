c72c25cc887334f48d44b9f57546a260fe694b41
Daniel Uboa <uboadaniel@gmail.com>
2022-04-30 14:38:55 +0300

    Basic numbers

---
+++ Day-01-Numbers_Arithmetic_and_Printing_to_Console/basic_numbers.py
@@ -0,0 +1,14 @@
+# In python, we have different ways of representing numbers,
+# The most commonn are integers and floats.
+
+# Integers
+5
+16
+57
+3999
+-7
+
+#Examples of floats
+4.0
+-15648.4
+3.14159
